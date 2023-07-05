7b609784dd64b42858a5cac1513e3e4e2d8ffe40
shadow <pieroramirez810@gmail.com>
2023-07-05 17:25:15 -0400

    fix: badname env in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const { baseURL: BASE_URL } = process.env
+const { BASE_URL } = process.env
 
 // https://vitejs.dev/config/
 export default defineConfig({
